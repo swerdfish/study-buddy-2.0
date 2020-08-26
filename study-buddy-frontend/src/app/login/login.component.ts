d3fb9a4e0b3beea41ceb07fd0fb4a8806229a09f
Ben Swerdlow <benswerdlow1005@gmail.com>
2020-08-25 20:37:30 -0400

    Minor changes in prep for ngrx

--- study-buddy-frontend/src/app/login/login.component.ts
+++ study-buddy-frontend/src/app/login/login.component.ts
@@ -11,6 +11,9 @@ import * as credKey from '../../../../../sheets-cred-key_studyBuddy.json'
 export class LoginComponent implements OnInit, AfterViewInit {
 
   clientId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 
   constructor(private router: Router) {
     this.clientId = credKey.client_id;
@@ -29,13 +32,19 @@ export class LoginComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     console.log("ngAfterViewInit");
+    let self = this;
     gapi.signin2.render('my-signin2', {
       scope: 'profile email',
       width: 240,
       height: 50,
       longtitle: true,
       theme: 'dark',
-      onsuccess: this.login,
+      onsuccess: (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        self.firstName = profile.getGivenName();
+        self.lastName = profile.getFamilyName();
+        self.email = profile.getEmail();
+      },
       onfailure: this.onFailure
     });
   }
